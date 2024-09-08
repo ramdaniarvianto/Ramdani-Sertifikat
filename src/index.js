b275132fce90e106fd11c27e31532062e80daa02
ramdaniarvianto <ramdaniarvianto1@gmail.com>
2024-09-08 13:18:17 +0700

    Merubah beberapa isi code, menghapus folder component, mengubah style

--- src/index.js
+++ src/index.js
@@ -1,2 +1,1 @@
 import './script/script.js';
-import './components/headLinks.js';
---
+++ src/script/script.js
@@ -0,0 +1,5 @@
+const close = document.querySelector('.close');
+close.addEventListener('click', function() {
+    const hilang = document.querySelector('.hilang');
+    hilang.style.display = 'none';
+});
